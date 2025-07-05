4f3cf3239362c541441eb78a913b5423e9860c83
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-05 19:46:12 +0200

    feat: record

---
+++ src/record-problem.ts
@@ -0,0 +1,17 @@
+export const createCache = () => {
+  const cache: Record<string, string> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
---
+++ tests/record-problem.test.ts
@@ -0,0 +1,19 @@
+import { expect, it } from "vitest";
+import { createCache } from "../src/record-problem";
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+
+  expect(cache.cache["123"]).toEqual("Matt");
+});
+
+it("Should remove values from the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
