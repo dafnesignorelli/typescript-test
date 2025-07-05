de9d14ce1128db5afb9e55c95d69d62ac267e627
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-05 20:20:47 +0200

    test: functiontype

---
+++ tests/functionType.test.ts
@@ -0,0 +1,19 @@
+import { expect, it } from "vitest";
+import { createThenGetUser } from "../src/functionType";
+
+it("Should create the user, then get them", async () => {
+  const user = await createThenGetUser(
+    async () => "123",
+    async (id) => ({
+      id,
+      firstName: "Matt",
+      lastName: "Pocock",
+    })
+  );
+
+  expect(user).toEqual({
+    id: "123",
+    firstName: "Matt",
+    lastName: "Pocock",
+  });
+});
