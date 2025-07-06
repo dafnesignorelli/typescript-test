06e4a989d72a116ef72ef5d7c5c726259f30df5a
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-06 13:52:58 +0200

    test: undestant classes

---
+++ src/understand.ts
@@ -0,0 +1,9 @@
+export class CanvasNode {
+    readonly x: number;
+    readonly y: number;
+
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+    }
+}
---
+++ tests/understand.test.ts
@@ -0,0 +1,15 @@
+import { expect, it } from "vitest";
+import { CanvasNode } from "../src/understand";
+
+it("Should store some basic properties", () => {
+  const canvasNode = new CanvasNode();
+
+  expect(canvasNode.x).toEqual(0);
+  expect(canvasNode.y).toEqual(0);
+
+  // @ts-expect-error Property is readonly
+  canvasNode.x = 10;
+
+  // @ts-expect-error Property is readonly
+  canvasNode.y = 20;
+});
