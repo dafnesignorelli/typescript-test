6a3a2ed9387aaf91fd24b3b3097649aa82ede1ce
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-06 19:06:01 +0200

    test: enums string

---
+++ src/stringEnums.ts
@@ -0,0 +1,10 @@
+export enum Method {
+    GET= "GET",
+    POST= "POST",
+    PUT= "PUT",
+    DELETE= "DELETE",
+  };
+  
+ export const request = (url: string, method: Method) => {
+    // ...
+  };
