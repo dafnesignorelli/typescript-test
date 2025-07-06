9bd138bddce6cb7881883fc275b0807a42ee41f4
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-06 19:40:26 +0200

    test: exemplos

---
+++ src/constEnum.ts
@@ -0,0 +1,48 @@
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+// var Direction;
+// (function (Direction) {
+//   Direction[(Direction["Up"] = 0)] = "Up";
+//   Direction[(Direction["Down"] = 1)] = "Down";
+//   Direction[(Direction["Left"] = 2)] = "Left";
+//   Direction[(Direction["Right"] = 3)] = "Right";
+// })(Direction || (Direction = {}));
+
+// const Direction = {
+//   0: "Up",
+//   Up: 0,
+//   1: "Down",
+//   Down: 1,
+//   2: "Left",
+//   Left: 2,
+//   3: "Right",
+//   Right: 3,
+// };
+
+// const enum Direction {
+//   Up,
+//   Down,
+//   Left,
+//   Right,
+// }
+
+// const directions = [
+//   Direction.Up,
+//   Direction.Down,
+//   Direction.Left,
+//   Direction.Right,
+// ];
+
+// Transpiles to:
+("use strict");
+let directions = [
+  "Up" /* Direction.Up */,
+  "Down" /* Direction.Down */,
+  "Left" /* Direction.Left */,
+  "Right" /* Direction.Right */,
+];
---
+++ src/namespace.ts
@@ -0,0 +1,29 @@
+namespace GeometryUtils {
+  export namespace Circle {
+    export function calculateArea(radius: number) {
+      // implementation
+    }
+
+    export function calculateCircumference(radius: number) {
+      // implementation
+    }
+  }
+
+  export namespace Rectangle {
+    export interface Rectangle {
+      width: number;
+      height: number;
+    }
+
+    export function calculateArea(rect: Rectangle) {
+      // implementation
+    }
+  }
+}
+
+GeometryUtils.Circle.calculateArea(10);
+
+const rect: GeometryUtils.Rectangle.Rectangle = {
+  width: 10,
+  height: 10,
+};
