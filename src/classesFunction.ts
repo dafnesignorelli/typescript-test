6e02c769071ce474b405172fbec4b3f7a97bf28c
dafnesignorelli <dafne.signorelli@gmail.com>
2025-07-06 14:01:06 +0200

    test: function classes

---
+++ src/classesFunction.ts
@@ -0,0 +1,14 @@
+export class CanvasNode {
+  x = 0;
+  y = 0;
+
+  public move(number1: number, number2: number) {
+    this.x = number1;
+    this.y = number2;
+  }
+
+  public move2 = (number1: number, number2: number) =>{
+    this.x = number1;
+    this.y = number2;
+  }
+}
---
+++ tests/classesFunction.test.ts
@@ -0,0 +1,14 @@
+import { expect, it } from "vitest";
+import {CanvasNode, } from "../src/classesFunction";
+
+it("Should be able to move", () => {
+  const canvasNode = new CanvasNode();
+
+  expect(canvasNode.x).toEqual(0);
+  expect(canvasNode.y).toEqual(0);
+
+  canvasNode.move(10, 20);
+
+  expect(canvasNode.x).toEqual(10);
+  expect(canvasNode.y).toEqual(20);
+});
